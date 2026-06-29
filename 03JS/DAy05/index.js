d66a5c65601043c6a2abb38ed51ffc41d6ed1162
Sumit Kumar <sumitknic2006@gmail.com>
2026-06-29 20:23:40 +0530

    today class

---
+++ 03JS/DAy05/index.js
@@ -0,0 +1,80 @@
+// Number
+// let a = 10;
+// let b = 345.68921
+// console.log(a);
+// console.log(b);
+
+// // toFixed
+// let c = b.toFixed(2);
+// console.log(c);
+// console.log(typeof c);
+
+// console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+
+// create number ke through
+// let a = new Number(20);
+// let b = new Number(20);  // false
+// // let b = 20; // true
+
+// console.log(a==b);
+// console.log(typeof b);
+// console.log(typeof a);
+
+let obj1 = {
+    name:"Sumit"
+}
+
+// let obj2 = obj1;  // true
+
+// let obj2 = {  // false
+//     name:"Sumit"
+// }
+
+// console.log(obj1==obj2);
+
+
+/* Math */
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.3));
+// console.log(Math.log10(20));
+// console.log(Math.pow(2,3));
+// console.log(Math.max(22,45,11,78,1));
+// console.log(Math.min(22,45,11,78,1));
+
+// console.log(Math.random());
+
+
+// Satebaaji ek game banata:(0-9)
+
+// console.log(Math.floor(Math.random()*10));
+
+// 1 to 10 value generate
+// console.log(Math.floor(Math.random()*10)+1);
+
+// 1 to 6
+// console.log(Math.floor(Math.random()*6)+1);
+// // or
+// console.log(Math.ceil(Math.random()*6));
+
+
+/* formula:- Math.floor(Math.random()*totalNumberOfOutcome)+shift  ,or
+=> Math.floor(Math.random()*(max-min+1))+min
+*/
+
+
+// 15-25
+
+// console.log(Math.floor(Math.random()*11)+15);
+
+
+/* OTP Generate: 4 digit: 1000-9999 */
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+
