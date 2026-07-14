060a85defcfd297e13fe7ad14416d7072e9bd484
Sumit Kumar <sumitknic2006@gmail.com>
2026-07-15 01:38:32 +0530

    day16/10

---
+++ 03JS/Day16/10/first.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const answer= {
+    q1: "Sachin Tendulkar",
+    q2: "West Indies",
+    q3: "Sachin Tendulkar",
+    q4: "264",
+    q5: "Muttiah Muralitharan"
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const data = new FormData(form);
+    let finalScore = 0;
+
+
+    for(let [name,value] of data.entries()){
+        if(answer[name]==value)
+            finalScore++;
+    }
+    
+
+    document.getElementById("out").textContent = `Your score is ${finalScore} out of 5`
+    
+    form.reset();
+
+})
